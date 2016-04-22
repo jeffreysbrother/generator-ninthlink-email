e7aeb409280207c36463fdb45ed088fcf47c7706
James Cool <jeffreysbrother@gmail.com>
2016-04-22 12:30:41 -0700

    updated initial message for testing purposes...app works just fine

--- app/index.js
+++ app/index.js
@@ -57,7 +57,7 @@ module.exports = generators.Base.extend({
     var done = this.async();
 
     if (!this.options['skip-welcome-message']) {
-      this.log(yosay('\'Allo \'allo! Out of the box I include HTML5 Boilerplate, jQuery, and a gulpfile to build your app.'));
+      this.log(yosay('Hey you wanna make a damn email? Well, that\'s SWELL.'));
     }
 
     var prompts = [{
